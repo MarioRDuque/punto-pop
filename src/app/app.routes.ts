caaaad3014e593e1a7171bf020dfe18dea2425da
Mario Mario <marioramos083@gmail.com>
2026-01-06 18:21:24 -0500

    integrar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './admin/component/app.layout';
+import { Dashboard } from './admin/modulos/dashboard/dashboard';
+import { Landing } from './admin/modulos/landing/landing';
+import { Notfound } from './admin/modulos/notfound/notfound';
+import { Documentation } from './admin/modulos/documentation/documentation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayout,
+        children: [
+            { path: '', component: Dashboard },
+            { path: 'uikit', loadChildren: () => import('./admin/modulos/uikit/uikit.routes') },
+            { path: 'documentation', component: Documentation },
+            { path: 'pages', loadChildren: () => import('./admin/modulos/pages.routes') }
+        ]
+    },
+    { path: 'landing', component: Landing },
+    { path: 'notfound', component: Notfound },
+    { path: 'auth', loadChildren: () => import('./admin/modulos/auth/auth.routes') },
+    { path: '**', redirectTo: '/notfound' }
+];
