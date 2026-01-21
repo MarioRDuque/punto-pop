e03a7a553bbf366bf2038cedd0378fae37d407c7
Mario Mario <marioramos083@gmail.com>
2026-01-21 18:57:03 -0500

    manejo de errores

---
+++ src/app/admin/interceptors/error.interceptor.ts
@@ -0,0 +1,64 @@
+import { inject } from '@angular/core';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { ToastService } from '../service/toast.service';
+
+export const errorInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+) => {
+
+  const messageService = inject(ToastService);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      switch (error.status) {
+        case 400:
+          if (error.error?.errors) {
+            //mejorar esto en un modal
+            let mensaje = Object.values(error.error.errors).join('\n');
+
+            messageService.error(
+              mensaje,
+              error.error?.title || 'Error de Negocio'
+            );
+          } else {
+            messageService.error(
+              error.error?.detail || 'Contacte al administrador',
+              'Error de Negocio'
+            );
+          }
+          break;
+        case 401:
+          messageService.warn(
+            error.error?.detail || 'Vuelve a iniciar sesión',
+            'Sesión expirada'
+          );
+          break;
+        case 403:
+          messageService.error(
+            error.error?.detail || 'No tienes permisos',
+            'Acceso denegado'
+          );
+          break;
+        default:
+          if (error.status >= 500) {
+            messageService.error(
+              error.error?.detail || 'Contacte al administrador',
+              'Error del servidor'
+            );
+          } else {
+            messageService.error(
+              error.error?.detail || 'Contacte al administrador',
+              'Error Desconocido'
+            );
+          }
+          break;
+      }
+
+      return throwError(() => error);
+
+    })
+  );
+};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,18 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { errorInterceptor } from './admin/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
-    MessageService
+    MessageService,
   ]
 };
