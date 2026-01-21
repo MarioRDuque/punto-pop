38ce06836a02e3dce931771acc64909df4ce5fcb
Mario Mario <marioramos083@gmail.com>
2026-01-20 19:15:49 -0500

    env

--- src/app/admin/modulos/configuracion/usuarios/usuarios.service.ts
+++ src/app/admin/modulos/configuracion/usuarios/usuarios.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../../service/api.service';
 import { Usuario } from '../../../entities/Usuario';
+import { environment } from '../../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,14 @@ import { Usuario } from '../../../entities/Usuario';
 export class UsuariosService {
   
   private api = inject(ApiService);
+  private readonly baseUrl = environment.apiUrl;
 
   obtenerUsuarios(): Observable<Usuario[]> {
-    return this.api.get<Usuario[]>('/usuarios');
+    return this.api.get<Usuario[]>('configuracion/usuario');
   }
 
   guardar(usuario: Usuario): Observable<void> {
-    return this.api.post<void>('/usuarios', usuario);
+    return this.api.post<void>('configuracion/usuario', usuario);
   }
   
 }
