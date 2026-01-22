9aeeac2448bde7a5565057a49038c37d411561b7
Mario Mario <marioramos083@gmail.com>
2026-01-21 22:31:55 -0500

    ag grid

--- src/app/admin/modulos/configuracion/usuarios/usuario-listado/usuario-listado.ts
+++ src/app/admin/modulos/configuracion/usuarios/usuario-listado/usuario-listado.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AgGridAngular } from "ag-grid-angular";
 import type { ColDef } from "ag-grid-community";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry, themeQuartz } from "ag-grid-community";
 import { HeaderCrud } from "../../../../component/header-crud/header-crud";
 import { myTheme } from '../../../../constantes/ag-grid-theme-builder';
 
@@ -21,23 +21,24 @@ interface IRow {
   styleUrl: './usuario-listado.scss',
 })
 export class UsuarioListado {
+  
   public theme: any = myTheme;
   titulo = 'Registro de Usuario';
   subtitulo = 'Usuarios';
-  
-  // Row Data: The data to be displayed.
-    rowData = [
-        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-        { make: "Ford", model: "F-Series", price: 33850, electric: false },
-        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    ];
 
-    // Column Definitions: Defines the columns to be displayed.
-    colDefs: ColDef[] = [
-        { field: "make" },
-        { field: "model" },
-        { field: "price" },
-        { field: "electric" }
-    ];
+  // Row Data: The data to be displayed.
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
 
 }
