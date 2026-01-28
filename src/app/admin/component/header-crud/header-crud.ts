694be8119047bf42a68d09e7e24bfec83b6098dd
Bryan Camacho <joelbermeo452@gmail.com>
2026-01-27 21:37:47 -0500

    filtros

--- src/app/admin/component/header-crud/header-crud.ts
+++ src/app/admin/component/header-crud/header-crud.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
+import { FormsData } from '../../service/forms-data';
 
 @Component({
   selector: 'app-header-crud',
@@ -11,7 +12,25 @@ import { PanelModule } from 'primeng/panel';
   styleUrl: './header-crud.scss',
 })
 export class HeaderCrud {
+
   @Input() titulo!: string;
   @Input() subtitulo!: string;
   @Input() esFormulario = false;
+  @Output() mostrarFiltro = new EventEmitter;
+  tabsState = inject(FormsData);
+  icons: string = "pi pi-filter";
+  tooltipSms: string ="Filtrar";
+
+
+  cambiarEstado() {
+    this.tabsState.cambiarEstado(!this.tabsState.esFiltrar());
+    if (this.tabsState.esFiltrar()) {
+      this.icons = "pi pi-filter-slash";
+      this.tooltipSms = "Ocultar filtro"
+    } else {
+      this.icons = "pi pi-filter";
+      this.tooltipSms = "Filtrar"
+    }
+  }
+
 }
