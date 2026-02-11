fbc32dcde9fd750d6e0451b2797512be448cca0d
Bryan Camacho <joelbermeo452@gmail.com>
2026-02-11 18:38:04 -0500

    Rol

--- src/app/admin/modulos/configuracion/configuracion.routes.ts
+++ src/app/admin/modulos/configuracion/configuracion.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppUsuarios } from './usuarios/app.usuarios';
+import { AppRol } from './rol/app.rol';
 
 export default [
     { path: 'usuarios', data: { breadcrumb: 'Usuario' }, component: AppUsuarios },
+    { path: 'rol', data: { breadcrumb: 'Rol' }, component: AppRol },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
---
+++ src/app/admin/modulos/configuracion/rol/app.rol.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FluidModule } from 'primeng/fluid';
+import { PanelModule } from 'primeng/panel';
+import { TabsModule } from 'primeng/tabs';
+import { RolFormulario } from "./rol-formulario/rol-formulario";
+import { RolListado } from "./rol-listado/rol-listado";
+import { TabsEnum } from '../../../enums/tabs-enum';
+import { TabsStateService } from '../../../service/tabs.service';
+import { FormsService } from '../../../service/forms-service';
+import { ICONSCONSTANT } from '../../../constantes/icons-constants';
+import { AccionEnum } from '../../../enums/accion-enum';
+
+@Component({
+  selector: 'app-rol',
+  imports: [
+    FluidModule,
+    PanelModule,
+    TabsModule,
+    RolFormulario,
+    RolListado
+  ],
+  templateUrl: './app.rol.html',
+  styleUrl: './app.rol.scss',
+})
+export class AppRol implements OnInit {
+  Tabs = TabsEnum;
+  tabsState = inject(TabsStateService);
+  formsService = inject(FormsService);
+  ICONSCONSTANT = ICONSCONSTANT;
+
+  ngOnInit(): void {
+    this.onTabChange(TabsEnum.LISTADO);
+  }
+
+  onTabChange(value: string | number | undefined) {
+    this.tabsState.irATab(value ?? '0');
+    if (value == this.Tabs.CREAR) {
+      this.formsService.accion.set(AccionEnum.CREAR);
+    }
+    this.tabsState.cambiarEstadoTab(true);
+  }
+}
